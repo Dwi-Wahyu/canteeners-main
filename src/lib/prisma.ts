1d6bc585cd3dce290ca7d2df790206ca95a04842
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-04-17 23:14:13 +0800

    disable prisma accelerate

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,3 @@
 import { PrismaClient } from "@/generated/prisma";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
-export const prisma = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL!,
-});
-
-export const prismaAccelerate = prisma.$extends(withAccelerate());
+export const prisma = new PrismaClient();
