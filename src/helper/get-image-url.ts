7d23ca33b9baf3a2eba882312fecf730f71c9fd9
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-04-28 11:29:12 +0800

    refactor: update getImageUrl function to ensure proper path formatting and add placeholder fallback

--- src/helper/get-image-url.ts
+++ src/helper/get-image-url.ts
@@ -1,3 +1,9 @@
 export function getImageUrl(path: string): string {
-  return process.env.NEXT_PUBLIC_BACKEND_URL + "/uploads" + path;
+  // if (!path) return "/placeholder-image.webp"; // fallback jika path kosong
+
+  // Memastikan path diawali dengan slash agar tidak terjadi double slash atau error path
+  const cleanPath = path.startsWith("/") ? path : `/${path}`;
+
+  // Karena sudah di-proxy oleh Nginx ke /uploads,
+  return `/uploads${cleanPath}`;
 }
