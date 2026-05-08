5dec2cf8585c5fc13f430d7422d51955e07b1c5b
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-05-09 06:14:47 +0800

    ?? process.env.DATABASE_URL

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,14 @@
 // prisma.config.ts
-import 'dotenv/config'
-import { defineConfig, env } from 'prisma/config'
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-    schema: 'prisma/schema/',
-    migrations: {
-        path: 'prisma/migrations',
-        seed: "bun run prisma/seed",
-    },
-    datasource: {
-        url: env('DATABASE_URL'),
-    },
-})
+  schema: "prisma/schema/",
+  migrations: {
+    path: "prisma/migrations",
+    seed: "bun run prisma/seed",
+  },
+  datasource: {
+    url: env("DATABASE_URL") ?? process.env.DATABASE_URL,
+  },
+});
